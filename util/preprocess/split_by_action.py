5479b3139a2444b42f9440aebf2bd310f4942207
PCMaster2165 <chrisbrett@shaw.ca>
2018-04-03 22:39:01 -0700

    fixed split_by_action.py

--- util/preprocess/split_by_action.py
+++ util/preprocess/split_by_action.py
@@ -12,27 +12,35 @@ label_list = [None]*16*2  # 15 actions * train and test
 for i in range(len(label_list)):
   label_list[i] = []
 
-for phase in ['train','val']:
+#for phase in ['train','val']:
+for phase in ['val']:
   for cam_id in cameras:
-    label_list_path = '/data/human3.6m/img_list/linux_accv_%s_%s_label_cropped_3d.txt' % (phase,cam_id);
+    label_list_path = 'G:\\CSC-486-Stuff-and-Things\\img_list\\linux_accv_%s_%s_label_cropped_3d.txt' % (phase,cam_id);
     label_list_file = open(label_list_path,'r')
     for lin in label_list_file.readlines():
-      elem = lin.split('/')
-      person_id = elem[6]
-      action_id = int(elem[7].split('_')[0]) - 1
+      item = lin.split(' ')
+      elem = item[0]
+      elem = elem.split('\\')
+      person_id = elem[5]
+      print(elem[6])
+      #print(elem[7].split('_')[0])
+      action_id = int(elem[6].split('_')[0]) - 1
+      
       if person_id in train_set:
         label_list[action_id].append(lin)
       elif person_id in test_set:
         label_list[action_id + 16].append(lin)
       else:
-        print "Error"
-        print person_id
+        print ("Error")
+        print (person_id)
 
-dst_folder = '/data/human3.6m/new_img_list/'
+dst_folder = 'G:\\CSC-486-Stuff-and-Things\\new_img_list'
 if not os.path.isdir(dst_folder):
   os.mkdir(dst_folder)
 
 for i in range(1,len(label_list)):
+  print(dst_label_list_path)
+  sys.exit(0)
   if i < 16:
     dst_label_list_path = dst_folder + 'train_' + str(i) + '.txt' 
   elif i == 16:
