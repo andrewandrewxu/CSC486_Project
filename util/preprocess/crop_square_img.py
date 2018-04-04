42f900639b01040c6b01636c0fdb10507a1834e5
PCMaster2165 <chrisbrett@shaw.ca>
2018-04-03 22:03:11 -0700

    working crop_square_img

--- util/preprocess/crop_square_img.py
+++ util/preprocess/crop_square_img.py
@@ -14,42 +14,43 @@ def square_bbox(bbox):
     border = (h-w) /2
     x =  max(0, x - border)
     w = h 
-  return [x,y,w,h]
+  return [int(x),int(y),int(w),int(h)]
 
-def crop_square_img(file_list,img_root, new_root_folder):
+def crop_square_img(file_list, new_root_folder):
   with open(file_list,'r') as bbox_file:
     for lin in bbox_file.readlines():
       elem = lin.split(' ')
       fn = elem[0]
       bbox = elem[1].split(',')
-      
-      fn_elem = fn.split('/') 
-      fn_prefix = '/'.join(fn_elem[4:-1])
+      fn_elem = fn.split('\\') 
+      fn_prefix = '\\'.join(fn_elem[4:-1])
       fn_name = fn_elem[-1]
-      new_prefix = new_root_folder   + '/'+ fn_prefix 
+      new_prefix = new_root_folder   + '\\'+ fn_prefix 
       if not os.path.isdir(new_prefix):
         os.makedirs(new_prefix)
-      new_fn = new_prefix + '/' + fn_name
-      bbox = map(lambda x:float(x), bbox)
+      new_fn = new_prefix + '\\' + fn_name
+      bbox = list(map(lambda x:float(x), bbox))
       
       
-      img_path = os.path.abspath(img_root  + '/' + fn)
+      #img_path = os.path.abspath(img_root  + '\' + fn)
+      img_path = os.path.abspath(fn)
       img = cv2.imread(img_path)
-      bbox = square_bbox(bbox)      
+      bbox = square_bbox(bbox)
       square_img = img[bbox[1]:bbox[1] + bbox[3],bbox[0]: bbox[0] + bbox[2]]
       cv2.imwrite(new_fn, square_img) 
 
 if __name__ == '__main__':
-  img_root = '/data/human3.6m/Release-v1.1/H36MDemo'
-  new_img_root = '/data/human3.6m/linux_square_imgs'
-  
+  #img_root = '/data/human3.6m/Release-v1.1/H36MDemo'
+  new_img_root = 'G:\\CSC-486-Stuff-and-Things\\linux_square_imgs'
+  #img_root = new_img_root
   if not os.path.isdir(new_img_root):
     os.mkdir(new_img_root)
 
-  for phase in ['train','val']:
+  #for phase in ['train','val']:
+  for phase in ['val']:
     for camera in ['1','2','3','4']:
-      file_list = '/data/human3.6m/img_list/linux_accv_%s_camera_%sbbox.txt'%(phase,camera)
-      crop_square_img(file_list, img_root, new_img_root)
+      file_list = 'G:\\CSC-486-Stuff-and-Things\\img_list\\linux_accv_%s_camera_%sbbox.txt'%(phase,camera)
+      crop_square_img(file_list, new_img_root)
 
   
       
